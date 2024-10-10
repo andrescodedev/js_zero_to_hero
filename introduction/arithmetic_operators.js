68ee21e8caa0b141b0c734bc5da9d1ac0800cbcb
andres hernandez <andrescodedev@gmail.com>
2024-10-10 17:47:16 -0500

    introduction section finalized successfuly

--- introduction/arithmetic_operators.js
+++ introduction/arithmetic_operators.js
@@ -33,7 +33,28 @@ Division: ${division}\nModule: ${module_oper}\nPotentiation: ${potentiation}`);*
 
 // PRESITION PROBLEMS
 let problem_presition = 0.1 + 0.2;
-console.log(`Presition Problem ${problem_presition}`);
-console.log(`Solution to presition problem ${problem_presition.toFixed(1)}`) //solution to presition problem
+//console.log(`Presition Problem ${problem_presition}`);
+//console.log(`Solution to presition problem ${problem_presition.toFixed(1)}`) //solution to presition problem
+
+
+
+//ADVANCED OPERATIONS WITH THE MATH LIBRARY
+let square_root = Math.sqrt(25);
+let absolute_value = Math.abs(-25);
+let random_value = Math.random();
+let random_value_from_zero = (max)=> Math.floor(Math.random() * max);
+let random_value_range = (min,max)=> Math.random() * (max-min) + min;
+let integer_random_value_range = (min,max)=> Math.floor(Math.random() * (max-min) + min);
+let integer_random_value_range_max_included = (min,max)=> Math.floor(Math.random() * (max-min+1) + min);
+
+
+console.log(`Square Root ${square_root}`);
+console.log(`Absolute Value ${absolute_value}`);
+console.log(`Random Value between 0 and 1 ${random_value}`); // expected: values between 0 and 1
+console.log(`Random Value between 0 and max value excluded ${random_value_from_zero(5)}`); // Expected: values between 0 and max excluded
+console.log(`Random Value between min value and max value excluded ${random_value_range(1,4)}`); // Expected: values betwee min and max excluded
+console.log(`Integer Random Value between min value and max value excluded ${integer_random_value_range(5,10)}`); // Expected: integer values between min and max excluded
+console.log(`Integer Random Value between min value and max value included ${integer_random_value_range_max_included(20,30)}`); // Expected: integer values between min and max included
+
 
 
