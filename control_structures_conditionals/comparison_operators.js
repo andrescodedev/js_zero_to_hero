ab3a53ef3af6cdf0c382a357903e942849b17220
andres hernandez <andrescodedev@gmail.com>
2024-10-11 19:45:27 -0500

    I worked on increment, decrement and assignment operators

---
+++ control_structures_conditionals/comparison_operators.js
@@ -0,0 +1,39 @@
+/* INCREMENT AND DECREMENT OPERATORS
+
+    ++ increment
+    -- decrement
+*/
+
+let increment_operator = 1;
+let decrement_operator = 10;
+
+
+console.log(`Increment by 1: ${increment_operator} -> ${++increment_operator}`);
+console.log(`Decrement by 1: ${decrement_operator} -> ${--decrement_operator}`);
+
+
+/*
+    ASSIGNMENT OPERATORS
+    
+    = 	    x = y 	    x = y
+    += 	    x += y 	    x = x + y
+    -=   	x -= y  	x = x - y
+    *=  	x *= y 	    x = x * y
+    /= 	    x /= y 	    x = x / y
+    %= 	    x %= y 	    x = x % y
+    **= 	x **= y 	x = x ** y
+*/
+
+let price = 20;
+let discount = 5;
+
+price -= discount;
+console.log(`Price with discount is ${price}`);
+
+let x = -100;
+x <<= 5;
+console.log(`left ${x}`);
+ 
+
+// COMPARISON OPERATORS
+
